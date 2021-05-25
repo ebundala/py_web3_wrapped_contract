857fb096d3dc83f5ed7b6cc6bc5b54d3cdcf50cf
Kristof <kristof.kovacs@telenav.com>
2021-05-25 11:48:09 +0300

    Update

--- web3_wrapped_contract/contract_utils.py
+++ web3_wrapped_contract/contract_utils.py
@@ -1,7 +1,7 @@
 # ------------------------------------------------------------ Imports ----------------------------------------------------------- #
 
 # Pip
-from web3._utils.abi import abi_to_signature
+from web3._utils.abi import normalize_event_input_types, abi_to_signature
 from web3.contract import ContractFunction
 
 # -------------------------------------------------------------------------------------------------------------------------------- #
@@ -13,7 +13,15 @@ from web3.contract import ContractFunction
 def method_signature(
     method: ContractFunction
 ) -> str:
-    return abi_to_signature(method.abi)
+    return "{fn_name}({fn_input_types})({fn_output_types})".format(
+        fn_name=method.abi['name'],
+        fn_input_types=','.join([
+            arg['type'] for arg in normalize_event_input_types(method.abi.get('inputs', []))
+        ]),
+        fn_output_types=','.join([
+            arg['type'] for arg in normalize_event_input_types(method.abi.get('outputs', []))
+        ])
+    )
 
 
 # -------------------------------------------------------------------------------------------------------------------------------- #
